04eb5d8859bc5eab6a377f82a310623fd2d90847
Krish Matta <self@krishxmatta.dev>
2022-02-14 13:31:16 -0500

    Setup facial recognition

--- wring/cli.py
+++ wring/cli.py
@@ -1,5 +1,8 @@
 import click
+import cv2
+import face_recognition
 import json
+import numpy as np
 import os
 import requests
 import time
@@ -13,6 +16,8 @@ from ring_doorbell import Ring, Auth
 def cli():
     log_print("Loading configuration...")
     config = load_config()
+    log_print("Loading faces...")
+    known_face_encodings, known_face_names = load_face_encodings(os.path.join(os.environ["HOME"], ".config/wring/img"))
     log_print("Connecting to Ring...")
     cache_dir_path = os.path.join(os.environ["HOME"], ".cache/wring")
     if not os.path.isdir(cache_dir_path):
@@ -32,6 +37,14 @@ def cli():
                 time.sleep(5)
                 download_video(doorbell, cache_dir_path)
                 log_print(f"Downloaded ring video for doorbell '{doorbell.name}'")
+                log_print(f"Analyzing video for faces...")
+                found_known_faces = recognize_video_faces(os.path.join(cache_dir_path, "curr_ding.mp4"), known_face_encodings, known_face_names)
+                for name in found_known_faces:
+                    if found_known_faces[name]:
+                        if name == "Unknown":
+                            log_print(f"An unknown person is at doorbell {doorbell.name}!")
+                        else:
+                            log_print(f"{name} is at {doorbell.name}!")
                 prev_ding_event_ids[doorbell.id] = curr_event["id"]
         time.sleep(1)
     
@@ -67,3 +80,45 @@ def download_video(doorbell, out_dir):
         for chunk in r.iter_content(chunk_size=1024):
             if chunk:
                 f.write(chunk)
+
+def load_face_encodings(img_dir):
+    known_face_encodings = []
+    known_face_names = []
+    for root, dirs, files in os.walk(img_dir):
+        if files:
+            for file in files:
+                known_face_encodings.append(face_recognition.face_encodings(face_recognition.load_image_file(os.path.join(root, file)))[0])
+                known_face_names.append(os.path.basename(root))
+    return known_face_encodings, known_face_names
+
+def recognize_video_faces(video_path, known_face_encodings, known_face_names):
+    cap = cv2.VideoCapture(video_path)
+    found_known_faces = {x: False for x in known_face_names}
+    found_known_faces["Unknown"] = False
+
+    process_frame = True
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if ret and known_face_encodings:
+            frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+            frame = frame[:, :, ::-1]
+            if process_frame:
+                face_locations = face_recognition.face_locations(frame)
+                face_encodings = face_recognition.face_encodings(frame, face_locations)
+                for face_encoding in face_encodings:
+                    matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+                    curr_name = "Unknown"
+
+                    face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+                    best_match_index = np.argmin(face_distances)
+                    if matches[best_match_index]:
+                        name = known_face_names[best_match_index]
+                        # Optimize facial recognition by removing already found faces
+                        known_face_encodings = known_face_encodings[:best_match_index] + known_face_encodings[best_match_index + 1:]
+                        known_face_names = known_face_names[:best_match_index] + known_face_names[best_match_index + 1:]
+                    found_known_faces[name] = True
+            process_frame = not process_frame
+        else:
+            break
+    cap.release()
+    return found_known_faces
